0fe6587a23a2ed0d3809df5bd2ca3be10bd7fcdd
Jeomon <jeogeoalukka@gmail.com>
2025-06-03 23:36:25 +0530

    examples updated

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ google_api_key=os.getenv('GOOGLE_API_KEY')
 config_path=Path.cwd()/'mcp_servers'/'config.json'
 client=MCPClient.from_config_file(config_path.as_posix())
 
-llm=ChatGemini(model='gemini-2.5-flash-preview-04-17',api_key=google_api_key,temperature=0.2)
+llm=ChatGemini(model='gemini-2.5-flash-preview-04-17',api_key=google_api_key,temperature=0)
 agent=MCPAgent(client=client,llm=llm,verbose=True,max_iteration=100)
 
 input=input('Enter a task: ')
